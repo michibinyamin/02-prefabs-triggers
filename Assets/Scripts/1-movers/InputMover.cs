25cec206758e1d4aedab3dc1784097cc3d558c77
michael <michael@xtendrobotics.com>
2024-11-25 23:19:39 +0200

    Created walls for game

--- Assets/Scripts/1-movers/InputMover.cs
+++ Assets/Scripts/1-movers/InputMover.cs
@@ -8,6 +8,15 @@ public class InputMover: MonoBehaviour {
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 10f;
 
+    [SerializeField] string triggeringTagFloor;  // For the walls
+    [SerializeField] string triggeringTagCeiling;  // For the walls
+    [SerializeField] string triggeringTagRight;  // For the walls
+    [SerializeField] string triggeringTagLeft;  // For the walls
+    private bool HitFloor = false;
+    private bool HitCeiling = false;
+    private bool HitRight = false;
+    private bool HitLeft = false;
+
     [SerializeField] InputAction move = new InputAction(
         type: InputActionType.Value, expectedControlType: nameof(Vector2));
 
@@ -22,6 +31,22 @@ public class InputMover: MonoBehaviour {
     void Update() {
         Vector2 moveDirection = move.ReadValue<Vector2>();
         Vector3 movementVector = new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
+        if (HitFloor && moveDirection.y < 0)
+        {
+            movementVector = new Vector3(moveDirection.x, 0, 0) * speed * Time.deltaTime;
+        }
+        if (HitCeiling && moveDirection.y > 0)
+        {
+            movementVector = new Vector3(moveDirection.x, 0, 0) * speed * Time.deltaTime;
+        }
+        if (HitRight)
+        {
+            transform.position = new Vector3(-10, transform.position.y, 0);
+        }
+        else if (HitLeft)
+        {
+            transform.position = new Vector3(10, transform.position.y, 0);
+        }
         transform.position += movementVector;
         //transform.Translate(movementVector);
         // NOTE: "Translate(movementVector)" uses relative coordinates - 
@@ -29,5 +54,43 @@ public class InputMover: MonoBehaviour {
         // In contrast, "transform.position += movementVector" would use absolute coordinates -
         //       it moves the object in the coordinate system of the world.
         // It makes a difference only if the object is rotated.
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTagFloor && enabled)
+        {
+            HitFloor = true;
+        }
+        if (other.tag == triggeringTagCeiling && enabled)
+        {
+            HitCeiling = true;
+        }
+        if (other.tag == triggeringTagRight && enabled)
+        {
+            HitRight = true;
+        }
+        if (other.tag == triggeringTagLeft && enabled)
+        {
+            HitLeft = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == triggeringTagFloor && enabled)
+        {
+            HitFloor = false;
+        }
+        if (other.tag == triggeringTagCeiling && enabled)
+        {
+            HitCeiling = false;
+        }
+        if (other.tag == triggeringTagRight && enabled)
+        {
+            HitRight = false;
+        }
+        if (other.tag == triggeringTagLeft && enabled)
+        {
+            HitLeft = false;
+        }
     }
 }
